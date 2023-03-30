5f27105a7d9d1891a038e48c07637508718625e0
no-ikjun <choiikjun1101@gmail.com>
2023-03-31 03:12:51 +0900

    some changes

--- pages/NavBar.tsx
+++ pages/NavBar.tsx
@@ -9,10 +9,11 @@ export default function NavBar() {
   const [isVisible, setIsVisible] = useState(false);
   const componentRef = useRef(null);
   const [themeMode, setThemeMode] = useState<"dark" | "light">("light");
+  const [isRotating, setIsRotating] = useState(false);
   const themeModeHandler = (e: { preventDefault: () => void }) => {
     e.preventDefault();
+    setIsRotating(true);
     setThemeMode(themeMode === "dark" ? "light" : "dark");
-    console.log(themeMode);
   };
 
   useEffect(() => {
@@ -28,12 +29,16 @@ export default function NavBar() {
         <Image className={[styles.logo_image, `${isVisible ? styles.logo_visible : styles.logo_unvisible}`].join(" ")} src="/Logo.webp" width={30} height={30} alt="Wing Logo" quality={100} />
         <Image className={[styles.logo_ment, `${isVisible ? styles.logo_visible : styles.logo_unvisible}`].join(" ")} src="/LogoMent.webp" width={90} height={30} alt="Wing Ment" quality={100} />
       </Link>
-      <div className={styles.nav_right}>
-        <div onClick={themeModeHandler}>
-          <Image className={styles.dark_mode} src="/moon.webp" width={25} height={25} alt="Dark Mode" quality={100} />
+      <div className={[styles.nav_right, `${isVisible ? styles.nav_right_visible : styles.nav_right_unvisible}`].join(" ")}>
+        <div onClick={themeModeHandler} className={isRotating ? "icon_div rotate" : "icon_div"}>
+          <Image className={styles.dark_mode} src={`/${themeMode === "light" ? "moon" : "sun"}.webp`} width={25} height={25} alt="Dark Mode" quality={100} />
         </div>
-        <Link href="/sessions">Sessions</Link>
-        <Link href="members">Members</Link>
+        <Link href="/sessions" className={styles.link}>
+          Sessions
+        </Link>
+        <Link href="members" className={styles.link}>
+          Members
+        </Link>
       </div>
     </nav>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import styles from "./modules/Home.module.css";
+import Image from "next/image";
 import { useState, useEffect, useRef } from "react";
 export default function Home() {
   const [windowSize, setWindowSize] = useState(0);
@@ -7,11 +8,32 @@ export default function Home() {
   }, []);
   return (
     <div>
-      <div className="main-div" style={{ background: "#FFFFF" }}>
-        <h1 className={styles.title}>WING</h1>
+      <div className={["main-div", styles.main_div].join(" ")} style={{ background: "#FFFFF" }}>
+        <div className={styles.sub_div} style={{ justifyContent: "flex-start", width: "40%" }}>
+          <h2 className={styles.sub_title}>
+            <span style={{ color: "#CD4233" }}>G</span>
+            <span style={{ color: "#5C625E" }}>IST</span>
+            <br />
+            Developer
+            <br />
+            Group
+          </h2>
+          <div className={styles.btn_div}>
+            <button className={styles.btn}>
+              Sessions<span>→</span>
+            </button>
+            <button className={styles.btn}>Members</button>
+          </div>
+        </div>
+        <div className={[styles.sub_div, "right_div", styles.show_later].join(" ")} style={{ alignItems: "center", width: "60%" }}>
+          <h1 className={styles.title}>WING</h1>
+        </div>
       </div>
+
       <div className="main-div" style={{ background: "#cccc" }}>
-        <h1 className={styles.title}>WING</h1>
+        <h1 className={styles.title} style={{ marginTop: 0 }}>
+          WING
+        </h1>
       </div>
 
       <style jsx>
@@ -21,7 +43,6 @@ export default function Home() {
             width: 100%;
             align-items: center;
             justify-content: center;
-            display: flex;
           }
         `}
       </style>
