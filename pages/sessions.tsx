703e399a8b499268c289dbc624e9037bc53a0378
no-ikjun <choiikjun1101@gmail.com>
2023-04-22 23:00:31 +0900

    session page mobile design

--- pages/sessions.tsx
+++ pages/sessions.tsx
@@ -27,7 +27,7 @@ export default function Sessions() {
             <span className={styles.sub_title}>from WING</span>
           </div>
           <div className={styles.sessions}>
-            <div className={[styles.sessions_div, styles.intro_text, styles.intro_left].join(" ")} style={{ width: "55%" }}>
+            <div className={[styles.sessions_div, styles.intro_text, styles.intro_left, styles.width_55].join(" ")}>
               <h2 className={styles.sessions_title}>What is WING Session?</h2>
               <p className={styles.sessions_content}>
                 WING 세션은 초보자부터 전문 개발자까지 코딩에 관심 있는 모든 분들을 대상으로 합니다. 우리의 목표는 회원들이 새로운 프로그래밍 기술을 배우고, 프로젝트를 협업하며, 서로 지식을 공유할 수
@@ -38,15 +38,15 @@ export default function Sessions() {
                 <strong> Senior 세션</strong>은 개발에 대한 경험이 있는 사람을 대상으로 진행하고 있습니다.
               </p>
             </div>
-            <div className={[styles.sessions_div, styles.intro_photo, styles.intro_right].join(" ")} style={{ width: "45%" }}>
+            <div className={[styles.sessions_div, styles.intro_photo, styles.intro_right, styles.width_45].join(" ")}>
               <Image className={styles.sessions_photo} src={"/kindpng_484211.webp"} alt="sessions_intro" width={500} height={300} />
             </div>
           </div>
           <div className={styles.sessions}>
-            <div className={[styles.sessions_div, styles.intro_photo, styles.intro_left].join(" ")} style={{ width: "35%" }}>
+            <div className={[styles.sessions_div, styles.intro_photo, styles.intro_left, styles.width_35].join(" ")}>
               <Image className={styles.sessions_photo} src={"/Daco_3737758.webp"} alt="sessions_intro" width={300} height={300} />
             </div>
-            <div className={[styles.sessions_div, styles.intro_text, styles.intro_right].join(" ")} style={{ width: "65%" }}>
+            <div className={[styles.sessions_div, styles.intro_text, styles.intro_right, styles.width_65].join(" ")}>
               <h2 className={styles.sessions_title}>How to join?</h2>
               <p className={styles.sessions_content}>
                 WING 세션은 개발에 관심있는 모두에게 열려있습니다. WING 세션에 참여하고 싶다면, 게시판에 공지된 포스터를 참고하여 시간에 맞게 <strong>해동학술정보실</strong>로 오시면 됩니다. 세션
