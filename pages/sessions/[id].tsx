15b0932762bffeecfbcbd591049f1dc610d57194
no-ikjun <choiikjun1101@gmail.com>
2023-05-11 09:24:13 +0900

    test : getStaticPaths return value test

--- pages/sessions/[id].tsx
+++ pages/sessions/[id].tsx
@@ -49,14 +49,12 @@ export default function SessionDetail({ session_data }: sessionProps) {
 export async function getStaticPaths() {
   const res = await axios.get(`https://wing-homepage.vercel.app/api/sessions`);
   const allPostsData = res.data;
-  const paths = allPostsData.map((post: any) => ({
-    params: {
-      id: post.id,
-    },
-  }));
+  const formData: { paths: { params: { id: number } }[] } = {
+    paths: allPostsData.map((post: { id: any }): any => ({ params: { id: post.id } })),
+  };
 
   return {
-    paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
+    formData,
     fallback: false,
   };
 }
